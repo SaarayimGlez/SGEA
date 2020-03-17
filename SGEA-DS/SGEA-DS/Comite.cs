fe5ca8d5f99a6259daeda9c0a207ffc73b9fc0c3
PXK Ü <puxka_shikof@hotmail.com>
2020-03-16 19:48:52 -0600

    Agregadas las ventanas del CU04 con sus funcionalidades sin conexión a BD

--- SGEA-DS/SGEA-DS/Comite.cs
+++ SGEA-DS/SGEA-DS/Comite.cs
@@ -3,15 +3,20 @@
     class Comite
     {
         private string nombre;
+        private string descripcion;
 
         public Comite(string nombre)
         {
             this.Nombre = nombre;
         }
 
-        public string Nombre
+        public Comite(string nombre, string descripcion)
         {
-            get => nombre; set => nombre = value;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
         }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
     }
 }
