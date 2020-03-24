256717687f7810330b26310f86bda5a9b790c72c
PXK Ü <puxka_shikof@hotmail.com>
2020-03-23 18:44:06 -0600

    Agregadas las ventanas del CU38 con sus funcionalidades sin conexión a BD

---
+++ SGEA-DS/SGEA-DS/Patrocinador.cs
@@ -0,0 +1,13 @@
+﻿namespace SGEA_DS
+{
+    internal class Patrocinador
+    {
+        public string nombre { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+        public string correoElectronico { get; set; }
+        public string empresa { get; set; }
+        public string direccion { get; set; }
+        public string numeroTelefono { get; set; }
+    }
+}
